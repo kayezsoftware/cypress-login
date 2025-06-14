88a459600f89103f86c127d9c297784a9d20c8a8
kayezsoftware <kartaca00@hotmail.com>
2025-06-15 02:14:50 +0300

    chore: add index.js with GitHub project URL

---
+++ index.js
@@ -0,0 +1,10 @@
+function cypressTestProjesi() {
+  const checklist = {
+    project_url: 'https://github.com/kayezsoftware/cypress-login',
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
